57eeb031df4fa9c10c9f59961687b700189ad2cd
CristianSotomayorGit <csotomayorcompsci@gmail.com>
2024-05-12 11:32:36 -0700

    implemented diagonial player movement

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 float playerPositionX;
 float playerPositionY;
 
+// Flags for movement direction
+int moveLeft = 0;
+int moveRight = 0;
+int moveUp = 0;
+int moveDown = 0;
+
 void drawPlayer() {
     glColor3f(1, 1, 0);
     glPointSize(8);
@@ -13,25 +19,55 @@ void drawPlayer() {
     glEnd();
 }
 
-void buttons(unsigned char key, int x, int y) {
-    
-    if (key == 'a') {
+void updatePosition() {
+    if (moveLeft) {
         playerPositionX -= 5;
     }
-
-    if (key == 'd') {
+    if (moveRight) {
         playerPositionX += 5;
     }
-
-    if (key == 'w') {
+    if (moveUp) {
         playerPositionY -= 5;
     }
-
-    if (key == 's') {
+    if (moveDown) {
         playerPositionY += 5;
     }
 
     glutPostRedisplay();
+}
+
+void buttons(unsigned char key, int x, int y) {
+    switch(key) {
+        case 'a':
+            moveLeft = 1;
+            break;
+        case 'd':
+            moveRight = 1;
+            break;
+        case 'w':
+            moveUp = 1;
+            break;
+        case 's':
+            moveDown = 1;
+            break;
+    }
+}
+
+void keyUp(unsigned char key, int x, int y) {
+    switch(key) {
+        case 'a':
+            moveLeft = 0;
+            break;
+        case 'd':
+            moveRight = 0;
+            break;
+        case 'w':
+            moveUp = 0;
+            break;
+        case 's':
+            moveDown = 0;
+            break;
+    }
 }
 
 void display() {
@@ -41,8 +77,8 @@ void display() {
 }
 
 void init() {
-    glClearColor(0.3, 0.3, 0.3, 0); //Background color
-    gluOrtho2D(0,1920,1080,0); //Window size
+    glClearColor(0.3, 0.3, 0.3, 0);
+    gluOrtho2D(0, 1920, 1080, 0);
     playerPositionX = 300;
     playerPositionY = 300;
 }
@@ -55,5 +91,7 @@ int main(int argc, char* argv[]) {
     init();
     glutDisplayFunc(display);
     glutKeyboardFunc(buttons);
+    glutKeyboardUpFunc(keyUp);  // Register the key up function
+    glutIdleFunc(updatePosition);  // Register the idle function to continuously update the position
     glutMainLoop();
 }
